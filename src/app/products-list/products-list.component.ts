aa4c189437172a75eb242e73ccffcfe96895f3d3
naeem2290 <naeemahmad2290@gmail.com>
2023-03-24 16:37:58 +0500

    Finailize code

--- src/app/products-list/products-list.component.ts
+++ src/app/products-list/products-list.component.ts
@@ -8,15 +8,15 @@ import { Router } from '@angular/router';
 })
 export class ProductsListComponent implements OnInit {
   public showMore: boolean = false;
-  loader: false;
+  loader = true;
 
   @Input() padding: any;
   constructor(public router: Router) {}
 
   ngOnInit(): void {
-    window.scroll(0,0);
+    window.scroll(0, 0);
 
-    setTimeout(()=>{
+    setTimeout(() => {
       this.loader = false;
     }, 2000);
   }
@@ -26,7 +26,7 @@ export class ProductsListComponent implements OnInit {
   }
 
   getProductList(): Array<Number> {
-    return this.showMore ? [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13] : [0, 1, 2, 3, 4];
+    return this.showMore ? [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] : [0, 1];
   }
 
   navigateToDetailPage() {
